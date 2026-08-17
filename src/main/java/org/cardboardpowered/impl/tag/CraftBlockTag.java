fc550504eefb16e91826f9b6c43055fb4567b65c
freadc0de <freadnight2@gmail.com>
2026-08-17 19:24:57 +0300

    Fix Bukkit material tags for modded registries

--- src/main/java/org/cardboardpowered/impl/tag/CraftBlockTag.java
+++ src/main/java/org/cardboardpowered/impl/tag/CraftBlockTag.java
@@ -1,55 +1,38 @@
 package org.cardboardpowered.impl.tag;
 
-import java.util.HashMap;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
+
 import net.minecraft.core.Registry;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
+
 import org.bukkit.Material;
 import org.bukkit.craftbukkit.util.CraftMagicNumbers;
 
 public class CraftBlockTag extends CraftTag<Block, Material> {
 
-    /*public BlockTagImpl(TagGroup<Block> registry, Identifier tag) {
-        super(registry, tag);
-    }
-
-    @Override
-    public boolean isTagged(Material item) {
-        try {
-            return getHandle().contains(CraftMagicNumbers.getBlock(item));
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    @Override
-    public Set<Material> getValues() {
-        HashMap<Material, Block> map = new HashMap<>();
-        for (Block block : getHandle().values()) {
-            try {
-                map.put(CraftMagicNumbers.getMaterial(block), block);
-            } catch (Exception e) {
-            }
-        }
-        return map.keySet();
-    }*/
-    
     public CraftBlockTag(Registry<Block> registry, TagKey<Block> tag) {
         super(registry, tag);
     }
 
+    @Override
     public boolean isTagged(Material item) {
         Block block = CraftMagicNumbers.getBlock(item);
+
         if (block == null) {
             return false;
         }
+
         return block.builtInRegistryHolder().is(this.tag);
     }
 
+    @Override
     public Set<Material> getValues() {
-        return this.getHandle().stream().map(block -> CraftMagicNumbers.getMaterial((Block)block.value())).collect(Collectors.toUnmodifiableSet());
+        return this.getHandle().stream()
+                .map(holder -> CraftMagicNumbers.getMaterial(holder.value()))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toUnmodifiableSet());
     }
-
 }
--- src/main/java/org/cardboardpowered/impl/tag/CraftItemTag.java
+++ src/main/java/org/cardboardpowered/impl/tag/CraftItemTag.java
@@ -1,44 +1,38 @@
 package org.cardboardpowered.impl.tag;
 
-import java.util.Collections;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
+
 import net.minecraft.core.Registry;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
+
 import org.bukkit.Material;
 import org.bukkit.craftbukkit.util.CraftMagicNumbers;
 
 public class CraftItemTag extends CraftTag<Item, Material> {
 
-    /*public ItemTagImpl(TagGroup<Item> registry, Identifier tag) {
-        super(registry, tag);
-    }
-
-    @Override
-    public boolean isTagged(Material item) {
-        return getHandle().contains(CraftMagicNumbers.getItem(item));
-    }
-
-    @Override
-    public Set<Material> getValues() {
-        return Collections.unmodifiableSet(getHandle().values().stream().map((item) -> CraftMagicNumbers.getMaterial(item)).collect(Collectors.toSet()));
-    }*/
-    
     public CraftItemTag(Registry<Item> registry, TagKey<Item> tag) {
         super(registry, tag);
     }
 
+    @Override
     public boolean isTagged(Material item) {
         Item minecraft = CraftMagicNumbers.getItem(item);
+
         if (minecraft == null) {
             return false;
         }
+
         return minecraft.builtInRegistryHolder().is(this.tag);
     }
 
+    @Override
     public Set<Material> getValues() {
-        return this.getHandle().stream().map(item -> CraftMagicNumbers.getMaterial((Item)item.value())).collect(Collectors.toUnmodifiableSet());
+        return this.getHandle().stream()
+                .map(holder -> CraftMagicNumbers.getMaterial(holder.value()))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toUnmodifiableSet());
     }
-
 }
