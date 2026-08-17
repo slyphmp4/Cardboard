dd90d64ace7330b37e7b5a7bc47467cc31917720
freadc0de <freadnight2@gmail.com>
2026-08-17 17:52:45 +0300

    Fix Fabric resource conditions registry loading

--- src/main/java/org/cardboardpowered/mixin/resources/RegistryLoadTaskMixin.java
+++ src/main/java/org/cardboardpowered/mixin/resources/RegistryLoadTaskMixin.java
@@ -1,34 +1,51 @@
 package org.cardboardpowered.mixin.resources;
 
 import java.util.Map;
+import java.util.stream.Stream;
 
+import com.mojang.serialization.Lifecycle;
+import io.papermc.paper.registry.PaperRegistryAccess;
+import net.minecraft.core.WritableRegistry;
+import net.minecraft.resources.RegistryDataLoader;
+import net.minecraft.resources.RegistryLoadTask;
+import net.minecraft.resources.RegistryLoadTask.PendingRegistration;
+import net.minecraft.resources.ResourceManagerRegistryLoadTask;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.mojang.serialization.Lifecycle;
-
-import io.papermc.paper.registry.PaperRegistryAccess;
-import net.minecraft.core.WritableRegistry;
-import net.minecraft.resources.RegistryDataLoader;
-import net.minecraft.resources.RegistryLoadTask;
-
 @Mixin(RegistryLoadTask.class)
-public class RegistryLoadTaskMixin {
+public abstract class RegistryLoadTaskMixin<T> {
 
 	@Shadow
-	public WritableRegistry registry;
-	
+	public WritableRegistry<T> registry;
+
 	@Inject(
-	        method = "<init>(Lnet/minecraft/resources/RegistryDataLoader$RegistryData;Lcom/mojang/serialization/Lifecycle;Ljava/util/Map;)V",
-	        at = @At("TAIL")
-	    )
-	    private void onConstructorTail(RegistryDataLoader.RegistryData<?> data, Lifecycle lifecycle, Map<?, ?> loadingErrors, CallbackInfo ci) {
-	        // Your custom logic code goes here
-	        // This runs at the very end of the constructor
-			PaperRegistryAccess.instance().registerRegistry(this.registry);
-	    }
-	
+			method = "<init>(Lnet/minecraft/resources/RegistryDataLoader$RegistryData;Lcom/mojang/serialization/Lifecycle;Ljava/util/Map;)V",
+			at = @At("TAIL")
+	)
+	private void cardboard$registerPaperRegistry(
+			RegistryDataLoader.RegistryData<T> data,
+			Lifecycle lifecycle,
+			Map<?, ?> loadingErrors,
+			CallbackInfo ci
+	) {
+		PaperRegistryAccess.instance().registerRegistry(this.registry);
+	}
+
+	@Inject(
+			method = "registerElements",
+			at = @At("TAIL")
+	)
+	private void cardboard$lockReferenceHolders(
+			Stream<PendingRegistration<T>> registrations,
+			CallbackInfo ci
+	) {
+		if ((Object) this instanceof ResourceManagerRegistryLoadTask<?>) {
+			PaperRegistryAccess.instance()
+					.lockReferenceHolders(this.registry.key());
+		}
+	}
 }
