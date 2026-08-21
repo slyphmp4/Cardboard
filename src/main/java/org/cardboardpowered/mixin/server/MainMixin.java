91937b02640d3266e0e1068de4628e67f5cbfe43
./slyph <118862741+slyphmp4@users.noreply.github.com>
2026-08-21 16:12:54 +0300

    Avoid anonymous OptionParser in MainMixin

--- src/main/java/org/cardboardpowered/mixin/server/MainMixin.java
+++ src/main/java/org/cardboardpowered/mixin/server/MainMixin.java
@@ -74,129 +74,128 @@ public class MainMixin {
 			org.cardboardpowered.BukkitLogger.getLogger().info("Loading Paper PluginInitializerManager..");
 	        
 	        try {
-	        	 OptionParser parser = new OptionParser() {
-	                 {
-	                     this.acceptsAll(asList("?", "help"), "Show the help");
+	        	OptionParser parser = new OptionParser();
 
-	                     this.acceptsAll(asList("c", "config"), "Properties file to use")
-	                             .withRequiredArg()
-	                             .ofType(File.class)
-	                             .defaultsTo(new File("server.properties"))
-	                             .describedAs("Properties file");
+	        	parser.acceptsAll(asList("?", "help"), "Show the help");
 
-	                     this.acceptsAll(asList("P", "plugins"), "Plugin directory to use")
-	                             .withRequiredArg()
-	                             .ofType(File.class)
-	                             .defaultsTo(new File("plugins"))
-	                             .describedAs("Plugin directory");
+	        	parser.acceptsAll(asList("c", "config"), "Properties file to use")
+	                    .withRequiredArg()
+	                    .ofType(File.class)
+	                    .defaultsTo(new File("server.properties"))
+	                    .describedAs("Properties file");
 
-	                     this.acceptsAll(asList("h", "host", "server-ip"), "Host to listen on")
-	                             .withRequiredArg()
-	                             .ofType(String.class)
-	                             .describedAs("Hostname or IP");
+	        	parser.acceptsAll(asList("P", "plugins"), "Plugin directory to use")
+	                    .withRequiredArg()
+	                    .ofType(File.class)
+	                    .defaultsTo(new File("plugins"))
+	                    .describedAs("Plugin directory");
 
-	                     this.acceptsAll(asList("W", "world-dir", "universe", "world-container"), "World container")
-	                             .withRequiredArg()
-	                             .ofType(File.class)
-	                             .defaultsTo(new File("."))
-	                             .describedAs("Directory containing worlds");
+	        	parser.acceptsAll(asList("h", "host", "server-ip"), "Host to listen on")
+	                    .withRequiredArg()
+	                    .ofType(String.class)
+	                    .describedAs("Hostname or IP");
 
-	                     this.acceptsAll(asList("w", "world", "level-name"), "World name")
-	                             .withRequiredArg()
-	                             .ofType(String.class)
-	                             .describedAs("World name");
+	        	parser.acceptsAll(asList("W", "world-dir", "universe", "world-container"), "World container")
+	                    .withRequiredArg()
+	                    .ofType(File.class)
+	                    .defaultsTo(new File("."))
+	                    .describedAs("Directory containing worlds");
 
-	                     this.acceptsAll(asList("p", "port", "server-port"), "Port to listen on")
-	                             .withRequiredArg()
-	                             .ofType(Integer.class)
-	                             .describedAs("Port");
+	        	parser.acceptsAll(asList("w", "world", "level-name"), "World name")
+	                    .withRequiredArg()
+	                    .ofType(String.class)
+	                    .describedAs("World name");
 
-	                     this.accepts("serverId", "Server ID")
-	                             .withRequiredArg();
+	        	parser.acceptsAll(asList("p", "port", "server-port"), "Port to listen on")
+	                    .withRequiredArg()
+	                    .ofType(Integer.class)
+	                    .describedAs("Port");
 
-	                     this.accepts("jfrProfile", "Enable JFR profiling");
+	        	parser.accepts("serverId", "Server ID")
+	                    .withRequiredArg();
 
-	                     this.accepts("pidFile", "pid File")
-	                             .withRequiredArg()
-	                             .withValuesConvertedBy(new PathConverter());
+	        	parser.accepts("jfrProfile", "Enable JFR profiling");
 
-	                     this.acceptsAll(asList("o", "online-mode"), "Whether to use online authentication")
-	                             .withRequiredArg()
-	                             .ofType(Boolean.class)
-	                             .describedAs("Authentication");
+	        	parser.accepts("pidFile", "pid File")
+	                    .withRequiredArg()
+	                    .withValuesConvertedBy(new PathConverter());
 
-	                     this.acceptsAll(asList("s", "size", "max-players"), "Maximum amount of players")
-	                             .withRequiredArg()
-	                             .ofType(Integer.class)
-	                             .describedAs("Server size");
+	        	parser.acceptsAll(asList("o", "online-mode"), "Whether to use online authentication")
+	                    .withRequiredArg()
+	                    .ofType(Boolean.class)
+	                    .describedAs("Authentication");
 
-	                     this.acceptsAll(asList("b", "bukkit-settings"), "File for bukkit settings")
-	                             .withRequiredArg()
-	                             .ofType(File.class)
-	                             .defaultsTo(new File("bukkit.yml"))
-	                             .describedAs("Yml file");
+	        	parser.acceptsAll(asList("s", "size", "max-players"), "Maximum amount of players")
+	                    .withRequiredArg()
+	                    .ofType(Integer.class)
+	                    .describedAs("Server size");
 
-	                     this.acceptsAll(asList("C", "commands-settings"), "File for command settings")
-	                             .withRequiredArg()
-	                             .ofType(File.class)
-	                             .defaultsTo(new File("commands.yml"))
-	                             .describedAs("Yml file");
+	        	parser.acceptsAll(asList("b", "bukkit-settings"), "File for bukkit settings")
+	                    .withRequiredArg()
+	                    .ofType(File.class)
+	                    .defaultsTo(new File("bukkit.yml"))
+	                    .describedAs("Yml file");
 
-	                     this.accepts("forceUpgrade", "Whether to force a world upgrade");
-	                     this.accepts("eraseCache", "Whether to force cache erase during world upgrade");
-	                     this.accepts("recreateRegionFiles", "Whether to recreate region files during world upgrade");
-	                     this.accepts("safeMode", "Loads level with vanilla datapack only"); // Paper
-	                     this.accepts("nogui", "Disables the graphical console");
+	        	parser.acceptsAll(asList("C", "commands-settings"), "File for command settings")
+	                    .withRequiredArg()
+	                    .ofType(File.class)
+	                    .defaultsTo(new File("commands.yml"))
+	                    .describedAs("Yml file");
 
-	                     this.accepts("nojline", "Disables jline and emulates the vanilla console");
+	        	parser.accepts("forceUpgrade", "Whether to force a world upgrade");
+	        	parser.accepts("eraseCache", "Whether to force cache erase during world upgrade");
+	        	parser.accepts("recreateRegionFiles", "Whether to recreate region files during world upgrade");
+	        	parser.accepts("safeMode", "Loads level with vanilla datapack only"); // Paper
+	        	parser.accepts("nogui", "Disables the graphical console");
 
-	                     this.accepts("noconsole", "Disables the console");
+	        	parser.accepts("nojline", "Disables jline and emulates the vanilla console");
 
-	                     this.acceptsAll(asList("v", "version"), "Show the CraftBukkit Version");
+	        	parser.accepts("noconsole", "Disables the console");
 
-	                     this.accepts("demo", "Demo mode");
+	        	parser.acceptsAll(asList("v", "version"), "Show the CraftBukkit Version");
 
-	                     this.accepts("bonusChest", "Enable the bonus chest");
+	        	parser.accepts("demo", "Demo mode");
 
-	                     this.accepts("initSettings", "Only create configuration files and then exit"); // SPIGOT-5761: Add initSettings option
+	        	parser.accepts("bonusChest", "Enable the bonus chest");
 
-	                     this.acceptsAll(asList("S", "spigot-settings"), "File for spigot settings")
-	                             .withRequiredArg()
-	                             .ofType(File.class)
-	                             .defaultsTo(new File("spigot.yml"))
-	                             .describedAs("Yml file");
+	        	parser.accepts("initSettings", "Only create configuration files and then exit"); // SPIGOT-5761: Add initSettings option
 
-	                     this.acceptsAll(asList("paper-dir", "paper-settings-directory"), "Directory for Paper settings")
-	                             .withRequiredArg()
-	                             .ofType(File.class)
-	                             .defaultsTo(new File(/*io.papermc.paper.configuration.PaperConfigurations.CONFIG_DIR*/ "config"))
-	                             .describedAs("Config directory");
+	        	parser.acceptsAll(asList("S", "spigot-settings"), "File for spigot settings")
+	                    .withRequiredArg()
+	                    .ofType(File.class)
+	                    .defaultsTo(new File("spigot.yml"))
+	                    .describedAs("Yml file");
 
-	                     this.acceptsAll(asList("paper", "paper-settings"), "File for Paper settings")
-	                             .withRequiredArg()
-	                             .ofType(File.class)
-	                             .defaultsTo(new File("paper.yml"))
-	                             .describedAs("Yml file");
+	        	parser.acceptsAll(asList("paper-dir", "paper-settings-directory"), "Directory for Paper settings")
+	                    .withRequiredArg()
+	                    .ofType(File.class)
+	                    .defaultsTo(new File(/*io.papermc.paper.configuration.PaperConfigurations.CONFIG_DIR*/ "config"))
+	                    .describedAs("Config directory");
 
-	                     this.acceptsAll(asList("add-plugin", "add-extra-plugin-jar"), "Specify paths to extra plugin jars to be loaded in addition to those in the plugins folder. This argument can be specified multiple times, once for each extra plugin jar path.")
-	                             .withRequiredArg()
-	                             .ofType(File.class)
-	                             .defaultsTo(new File[] {})
-	                             .describedAs("Jar file");
+	        	parser.acceptsAll(asList("paper", "paper-settings"), "File for Paper settings")
+	                    .withRequiredArg()
+	                    .ofType(File.class)
+	                    .defaultsTo(new File("paper.yml"))
+	                    .describedAs("Yml file");
 
-	                     this.acceptsAll(asList("add-plugin-dir", "add-extra-plugin-dir"), "Specify paths to extra plugin directories to be loaded in addition to the plugins folder. This argument can be specified multiple times, once for each extra plugin dir path.")
-	                             .withRequiredArg()
-	                             .ofType(File.class)
-	                             .defaultsTo(new File[] {})
-	                             .describedAs("Plugin directory");
+	        	parser.acceptsAll(asList("add-plugin", "add-extra-plugin-jar"), "Specify paths to extra plugin jars to be loaded in addition to those in the plugins folder. This argument can be specified multiple times, once for each extra plugin jar path.")
+	                    .withRequiredArg()
+	                    .ofType(File.class)
+	                    .defaultsTo(new File[] {})
+	                    .describedAs("Jar file");
 
-	                     this.accepts("server-name", "Name of the server")
-	                             .withRequiredArg()
-	                             .ofType(String.class)
-	                             .defaultsTo("Unknown Server")
-	                             .describedAs("Name");
-	                 }
-	             };
+	        	parser.acceptsAll(asList("add-plugin-dir", "add-extra-plugin-dir"), "Specify paths to extra plugin directories to be loaded in addition to the plugins folder. This argument can be specified multiple times, once for each extra plugin dir path.")
+	                    .withRequiredArg()
+	                    .ofType(File.class)
+	                    .defaultsTo(new File[] {})
+	                    .describedAs("Plugin directory");
+
+	        	parser.accepts("server-name", "Name of the server")
+	                    .withRequiredArg()
+	                    .ofType(String.class)
+	                    .defaultsTo("Unknown Server")
+	                    .describedAs("Name");
+
 	        	OptionSet options = parser.parse(strings);
 	        	CardboardMod.options = options;
 				// PluginInitializerManager.load(options);
