5b62d59716f5a7d6dc0fa780ea7ca527e79272a5
./slyph <118862741+slyphmp4@users.noreply.github.com>
2026-09-05 21:40:27 +0300

    Strengthen Display spawning regression coverage

--- src/test/java/org/bukkit/craftbukkit/entity/CraftEntityTypesDisplayTest.java
+++ src/test/java/org/bukkit/craftbukkit/entity/CraftEntityTypesDisplayTest.java
@@ -13,12 +13,9 @@ class CraftEntityTypesDisplayTest {
         String entityTypes = Files.readString(Path.of("src/main/java/org/bukkit/craftbukkit/entity/CraftEntityTypes.java"));
         String craftWorld = Files.readString(Path.of("src/main/java/org/cardboardpowered/impl/world/CraftWorld.java"));
 
-        assertTrue(entityTypes.contains("EntityType.TEXT_DISPLAY, TextDisplay.class, CraftTextDisplay::new"));
-        assertTrue(entityTypes.contains("EntityTypes.TEXT_DISPLAY"));
-        assertTrue(entityTypes.contains("EntityType.ITEM_DISPLAY, ItemDisplay.class, CraftItemDisplay::new"));
-        assertTrue(entityTypes.contains("EntityTypes.ITEM_DISPLAY"));
-        assertTrue(entityTypes.contains("EntityType.BLOCK_DISPLAY, BlockDisplay.class, CraftBlockDisplay::new"));
-        assertTrue(entityTypes.contains("EntityTypes.BLOCK_DISPLAY"));
+        assertTrue(entityTypes.contains("EntityType.TEXT_DISPLAY, TextDisplay.class, CraftTextDisplay::new, combine(createAndSetPos(net.minecraft.world.entity.EntityTypes.TEXT_DISPLAY), ROT)"));
+        assertTrue(entityTypes.contains("EntityType.ITEM_DISPLAY, ItemDisplay.class, CraftItemDisplay::new, combine(createAndSetPos(net.minecraft.world.entity.EntityTypes.ITEM_DISPLAY), ROT)"));
+        assertTrue(entityTypes.contains("EntityType.BLOCK_DISPLAY, BlockDisplay.class, CraftBlockDisplay::new, combine(createAndSetPos(net.minecraft.world.entity.EntityTypes.BLOCK_DISPLAY), ROT)"));
         assertTrue(craftWorld.contains("Display.class.isAssignableFrom(clazz)"));
         assertTrue(craftWorld.contains("? super.createEntity(location, clazz, true)"));
     }
